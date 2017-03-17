6c9e3c5ec1e2a67b219d672774a21736dab76055
zhangli <footman265@gmail.com>
2017-03-17 23:46:18 +0800

    init

--- src/main/java/com/fm/java/nio/MultiPortEcho.java
+++ src/main/java/com/fm/java/nio/MultiPortEcho.java
@@ -11,6 +11,14 @@ import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
 
+/**
+ * 利用异步IO在多个端口上接受输入，再原样输出
+ *
+ * 这个程序有点过于简单，因为它的目的只是展示异步 I/O 所涉及的技术。在现实的应
+ * 用程序中，您需要通过将通道从 Selector 中删除来处理关闭的通道。而且您可能要
+ * 使用多个线程。这个程序可以仅使用一个线程，因为它只是一个演示，但是在现实场景
+ * 中，创建一个线程池来负责 I/O 事件处理中的耗时部分会更有意义。
+ */
 public class MultiPortEcho {
     private int ports[];
     private ByteBuffer echoBuffer = ByteBuffer.allocate(1024);
@@ -40,6 +48,11 @@ public class MultiPortEcho {
         }
 
         while (true) {
+            /**
+             * 首先，我们调用 Selector 的 select() 方法。这个方法会阻塞，直到至少有一个
+             * 已注册的事件发生。当一个或者更多的事件发生时， select() 方法将返回所发生的
+             * 事件的数量。
+             */
             int num = selector.select();
 
             Set selectedKeys = selector.selectedKeys();
@@ -48,8 +61,7 @@ public class MultiPortEcho {
             while (it.hasNext()) {
                 SelectionKey key = (SelectionKey) it.next();
 
-                if ((key.readyOps() & SelectionKey.OP_ACCEPT)
-                        == SelectionKey.OP_ACCEPT) {
+                if ((key.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT) {
                     // Accept the new connection
                     ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
                     SocketChannel sc = ssc.accept();
@@ -60,8 +72,7 @@ public class MultiPortEcho {
                     it.remove();
 
                     System.out.println("Got connection from " + sc);
-                } else if ((key.readyOps() & SelectionKey.OP_READ)
-                        == SelectionKey.OP_READ) {
+                } else if ((key.readyOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ) {
                     // Read the data
                     SocketChannel sc = (SocketChannel) key.channel();
 
@@ -89,9 +100,9 @@ public class MultiPortEcho {
 
             }
 
-//System.out.println( "going to clear" );
-//      selectedKeys.clear();
-//System.out.println( "cleared" );
+            //System.out.println( "going to clear" );
+            //selectedKeys.clear();
+            //System.out.println( "cleared" );
         }
     }
 
--- src/main/java/com/fm/java/nio/UseCharsets.java
+++ src/main/java/com/fm/java/nio/UseCharsets.java
@@ -1,39 +1,44 @@
 package com.fm.java.nio;// $Id$
 
-import java.io.*;
-import java.nio.*;
-import java.nio.channels.*;
-import java.nio.charset.*;
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
 
-public class UseCharsets
-{
-  static public void main( String args[] ) throws Exception {
-    String inputFile = "samplein.txt";
-    String outputFile = "sampleout.txt";
+public class UseCharsets {
 
-    RandomAccessFile inf = new RandomAccessFile( inputFile, "r" );
-    RandomAccessFile outf = new RandomAccessFile( outputFile, "rw" );
-    long inputLength = new File( inputFile ).length();
+    static public void main(String args[]) throws Exception {
+        String inputFile = "samplein.txt";
+        String outputFile = "sampleout.txt";
 
-    FileChannel inc = inf.getChannel();
-    FileChannel outc = outf.getChannel();
+        RandomAccessFile inf = new RandomAccessFile(inputFile, "r");
+        RandomAccessFile outf = new RandomAccessFile(outputFile, "rw");
+        long inputLength = new File(inputFile).length();
 
-    MappedByteBuffer inputData =
-      inc.map( FileChannel.MapMode.READ_ONLY, 0, inputLength );
+        FileChannel inc = inf.getChannel();
+        FileChannel outc = outf.getChannel();
 
-    Charset latin1 = Charset.forName( "ISO-8859-1" );
-    CharsetDecoder decoder = latin1.newDecoder();
-    CharsetEncoder encoder = latin1.newEncoder();
+        MappedByteBuffer inputData = inc.map(FileChannel.MapMode.READ_ONLY, 0, inputLength);
 
-    CharBuffer cb = decoder.decode( inputData );
+        Charset latin1 = Charset.forName("ISO-8859-1");
+        CharsetDecoder decoder = latin1.newDecoder();
+        CharsetEncoder encoder = latin1.newEncoder();
 
-    // Process char data here
+        CharBuffer cb = decoder.decode(inputData);
 
-    ByteBuffer outputData = encoder.encode( cb );
+        // Process char data here
 
-    outc.write( outputData );
+        ByteBuffer outputData = encoder.encode(cb);
 
-    inf.close();
-    outf.close();
-  }
+        outc.write(outputData);
+
+        inf.close();
+        outf.close();
+    }
+
 }
