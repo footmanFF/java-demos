d0282c64532c4942259b9c07292ded56eb24d4fa
zhangli <footman265@gmail.com>
2017-03-17 23:14:47 +0800

    init

--- src/main/java/com/fm/java/nio/UseFileLocks.java
+++ src/main/java/com/fm/java/nio/UseFileLocks.java
@@ -1,33 +1,36 @@
 package com.fm.java.nio;// $Id$
 
-import java.io.*;
-import java.nio.*;
-import java.nio.channels.*;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
-public class UseFileLocks
-{
-  static private final int start = 10;
-  static private final int end = 20;
+public class UseFileLocks {
 
-  static public void main( String args[] ) throws Exception {
-    // Get file channel
-    RandomAccessFile raf = new RandomAccessFile( "usefilelocks.txt", "rw" );
-    FileChannel fc = raf.getChannel();
+    static private final int start = 10;
+    static private final int end = 20;
 
-    // Get lock
-    System.out.println( "trying to get lock" );
-    FileLock lock = fc.lock( start, end, false );
-    System.out.println( "got lock!" );
+    static public void main(String args[]) throws Exception {
+        // Get file channel
+        RandomAccessFile raf = new RandomAccessFile("usefilelocks.txt", "rw");
+        FileChannel fc = raf.getChannel();
 
-    // Pause
-    System.out.println( "pausing" );
-    try { Thread.sleep( 3000 ); } catch( InterruptedException ie ) {}
+        // Get lock
+        System.out.println("trying to get lock");
+        FileLock lock = fc.lock(start, end, false);
+        System.out.println("got lock!");
 
-    // Release lock
-    System.out.println( "going to release lock" );
-    lock.release();
-    System.out.println( "released lock" );
+        // Pause
+        System.out.println("pausing");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ie) {
+        }
 
-    raf.close();
-  }
+        // Release lock
+        System.out.println("going to release lock");
+        lock.release();
+        System.out.println("released lock");
+
+        raf.close();
+    }
 }
