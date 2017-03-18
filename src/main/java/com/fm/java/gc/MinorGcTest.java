42647a3342ccec562ee07015ebcc95257e6e4f49
zhangli <footman265@gmail.com>
2017-03-19 00:10:26 +0800

    init

--- src/main/java/com/fm/java/gc/MinorGcTest.java
+++ src/main/java/com/fm/java/gc/MinorGcTest.java
@@ -7,13 +7,18 @@ import java.util.concurrent.TimeUnit;
  */
 public class MinorGcTest {
 
-    /**
-     * -Xms2000m -Xmx2000m 下，疯狂minor gc，4核mac下cpu占用为10%
-     */
     public static void main(String[] args) throws Exception{
         while(true){
             byte[] bytes = new byte[200 * 1024 * 1024];
-            TimeUnit.MILLISECONDS.sleep(200L);
+            /**
+             * -Xms2000m -Xmx2000m 下，疯狂minor gc，4核mac下cpu占用
+             * sleep 200L 为10%
+             * sleep 50L 为30%
+             * sleep 0   为90+%
+             *
+             * 新生代使用 Parallel scavenge收集器，gc时候会stop the world，然后进行多线程gc
+             */
+            TimeUnit.MILLISECONDS.sleep(50L);
         }
     }
 
