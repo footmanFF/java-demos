e65740a7a7eb02a21e47a3f3269b4dfcd3d6c67d
zhangli <footman265@gmail.com>
2017-03-20 22:10:32 +0800

    生产者消费者问题

--- src/main/java/com/fm/java/concurrent/FIFOQueue.java
+++ src/main/java/com/fm/java/concurrent/FIFOQueue.java
@@ -14,9 +14,13 @@ public class FIFOQueue {
     private int max = 10;
 
     /**
-     * 这里用while而不是if的目的是为了让wait被唤醒以后，继续执行下面的逻辑之前保证condition是正确的
+     * <p>这里用while而不是if的目的是为了让wait被唤醒以后，继续执行下面的逻辑之前保证condition是正确的
      * 有可能有其他的线程调用了某个方法，在wait被唤醒的刹那改变了condition，如果是if的话，wait被唤醒
      * 之后就无法保证程序逻辑正确了
+     *
+     * <p>还有一种情况是当其他根本不是一个业务的方法调用在对象上调用了notify，那么while(condition)保证了
+     * 即使被唤醒也会继续进入等待状态，程序逻辑是一致的
+     *
      * 《Java 编程思想》711页
      * @param item
      * @throws InterruptedException
