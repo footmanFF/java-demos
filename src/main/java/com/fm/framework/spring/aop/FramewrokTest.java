02b4ee6aaf7981c15ff71760af10fa8b132517d2
zhangli <footman265@gmail.com>
2017-04-23 22:35:36 +0800

    Spring AOP demos

---
+++ src/main/java/com/fm/framework/spring/aop/FramewrokTest.java
@@ -0,0 +1,27 @@
+package com.fm.framework.spring.aop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by fm on 2017/4/23.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring.xml"})
+public class FramewrokTest {
+
+    @Autowired
+    private SomeBean someBean;
+
+    @Test
+    public void test(){
+        someBean.task();
+        someBean.doSomeThing("hello world");
+        someBean.doReturn();
+        someBean.doThrow();
+    }
+
+}
