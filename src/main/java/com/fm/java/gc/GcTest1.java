0ce882a3e24905f83f5e1412a74027fc43db5027
zhangli <footman265@gmail.com>
2017-11-14 15:01:42 +0800

    线程池监控 demo

--- src/main/java/com/fm/java/gc/GcTest1.java
+++ src/main/java/com/fm/java/gc/GcTest1.java
@@ -7,17 +7,27 @@ import org.junit.Test;
  */
 public class GcTest1 {
     
-    @Test
-    public void test1(){
-        byte[] a = new byte[6 * 1024 * 1024];
-        System.gc();
-    }
-
+//    @Test
+//    public void test1(){
+//        byte[] a = new byte[6 * 1024 * 1024];
+//        System.gc();
+//    }
+//
     @Test
     public void test2(){
         byte[] a = new byte[6 * 1024 * 1024];
         a = null;
+
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < 1000000; i++) {
+            s.append(i).toString().intern();
+        }
         System.gc();
+    }
+
+    public static void main(String[] args) {
+        String s = new String("AAA");
+        System.out.println(s);
     }
     
 }
