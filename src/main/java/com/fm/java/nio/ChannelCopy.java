201296ae7e8cb406c9b41389007ddbab730119ca
zhangli <footman265@gmail.com>
2017-11-20 21:42:21 +0800

    nio channel

--- src/main/java/com/fm/java/nio/ChannelCopy.java
+++ src/main/java/com/fm/java/nio/ChannelCopy.java
@@ -7,32 +7,33 @@ import java.nio.channels.Channels;
 import java.io.IOException;
 
 /**
- * Test copying between channels. *
+ * Test copying between channels. 
  * <p>
- * 58 * @author Ron Hitchens (ron@ronsoft.com)
+ * @author Ron Hitchens (ron@ronsoft.com)
  */
 public class ChannelCopy {
     /**
-     * This code copies data from stdin to stdout. Like the 'cat' * command, but without
+     * This code copies data from stdin to stdout. Like the 'cat' command, but without
      * any useful options.
      */
     public static void main(String[] argv) throws IOException {
         ReadableByteChannel source = Channels.newChannel(System.in);
         WritableByteChannel dest = Channels.newChannel(System.out);
+        
+        // alternatively, call channelCopy2 (source, dest); 
         channelCopy1(source, dest);
 
-        // alternatively, call channelCopy2 (source, dest); 
         source.close();
         dest.close();
     }
 
     /**
-     * Channel copy method 1. This method copies data from the src * channel and writes it to
+     * Channel copy method 1. This method copies data from the src channel and writes it to
      * the dest channel until EOF on src.
      * <p>
-     * This implementation makes use of compact( ) on the temp buffer * to pack down the data
-     * if the buffer wasn't fully drained. This * may result in data copying, but minimizes
-     * system calls. It also * requires a cleanup loop to make sure all the data gets sent.
+     * This implementation makes use of compact( ) on the temp buffer to pack down the data
+     * if the buffer wasn't fully drained. This may result in data copying, but minimizes
+     * system calls. It also requires a cleanup loop to make sure all the data gets sent.
      */
     private static void channelCopy1(ReadableByteChannel src, WritableByteChannel dest) throws IOException {
         ByteBuffer buffer = ByteBuffer.allocateDirect(16 * 1024);
@@ -55,9 +56,9 @@ public class ChannelCopy {
     }
 
     /**
-     * Channel copy method 2. This method performs the same copy, but * assures the temp buffer
-     * is empty before reading more data. This * never requires data copying but may result in
-     * more systems calls. * No post-loop cleanup is needed because the buffer will be empty *
+     * Channel copy method 2. This method performs the same copy, but assures the temp buffer
+     * is empty before reading more data. This never requires data copying but may result in
+     * more systems calls. No post-loop cleanup is needed because the buffer will be empty
      * when the loop is exited.
      */
     private static void channelCopy2(ReadableByteChannel src, WritableByteChannel dest) throws IOException {
