f8a92f78ab37326b964dde0d2af4434bbccf2233
footmanff <footman265@gmail.com>
2017-12-15 13:53:21 +0800

    测试

--- src/main/java/com/fm/java/nio/NioDemos.java
+++ src/main/java/com/fm/java/nio/NioDemos.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.io.FileInputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.Selector;
 
 /**
  * Created by zhangli on 2017/5/31.
@@ -21,4 +22,22 @@ public class NioDemos {
         fileChannel.read(buffer);
     }
 
+    @Test
+    public void test2() throws Exception {
+        Selector selector = Selector.open();
+
+//        channel1.register(selector, SelectionKey.OP_READ);
+//        channel2.register(selector, SelectionKey.OP_WRITE);
+//        channel3.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+//
+//        // Wait up to 10 seconds for a channel to become ready 
+//        readyCount = selector.select(10000);
+
+    }
+
+    @Test
+    public void name() throws Exception {
+        System.out.println(1 << 2);
+    }
+    
 }
