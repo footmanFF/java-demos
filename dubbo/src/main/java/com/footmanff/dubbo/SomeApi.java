4fb5fbbbebc13a8aecaa1966e8ca28e994248576
footmanff <footman265@gmail.com>
2018-01-04 21:03:59 +0800

    dubbo异步

---
+++ dubbo/src/main/java/com/footmanff/dubbo/SomeApi.java
@@ -0,0 +1,10 @@
+package com.footmanff.dubbo;
+
+/**
+ * @author zhangli on 2018/1/4.
+ */
+public interface SomeApi {
+    
+    String call();
+    
+}
---
+++ dubbo/src/main/java/com/footmanff/dubbo/SomeApiImpl.java
@@ -0,0 +1,16 @@
+package com.footmanff.dubbo;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhangli on 2018/1/4.
+ */
+@Component("someApi")
+public class SomeApiImpl implements SomeApi {
+
+    @Override
+    public String call() {
+        return "hello";
+    }
+    
+}
