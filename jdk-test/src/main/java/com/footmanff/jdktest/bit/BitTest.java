57060d8723aee4ba6b748713350aa0cddc1d1c8d
footmanff <footman265@gmail.com>
2018-03-30 15:22:50 +0800

    bit test

--- jdk-test/src/main/java/com/footmanff/jdktest/bit/BitTest.java
+++ jdk-test/src/main/java/com/footmanff/jdktest/bit/BitTest.java
@@ -1,8 +1,6 @@
 package com.footmanff.jdktest.bit;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -13,7 +11,6 @@ public class BitTest {
     @Test
     public void test1() {
         assertEquals(1 << 1, 2);
-
         assertEquals(2 >> 1, 1);
     }
 
@@ -29,5 +26,45 @@ public class BitTest {
             }
         }
     }
+    
+    /**
+     * ForkJoinPool.externalSubmit中对workQueues初始化中，对数组容量的计算测试
+     * <p>
+     * n |= n >>> 1; n |= n >>> 2;  n |= n >>> 4;
+     * n |= n >>> 8; n |= n >>> 16; n = (n + 1) << 1;
+     */
+    @Test
+    public void test3() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("n init: " + i);
+            createN(i);
+            System.out.println();
+            System.out.println();
+        }
+    }
 
+    private int createN(int n) {
+        n |= n >>> 1;
+        System.out.println("n |= n >>> 1     ------- " + n);
+        n |= n >>> 2;
+        System.out.println("n |= n >>> 2     ------- " + n);
+        n |= n >>> 4;
+        System.out.println("n |= n >>> 4     ------- " + n);
+        n |= n >>> 8;
+        System.out.println("n |= n >>> 8     ------- " + n);
+        n |= n >>> 16;
+        System.out.println("n |= n >>> 16    ------- " + n);
+        n = (n + 1) << 1;
+        System.out.println("n = (n + 1) << 1 ------- " + n);
+        return n;
+    }
+    
+    @Test
+    public void test4() {
+        int a = 14;
+        int b = a >>> 2;
+        System.out.println(b);
+        System.out.println(a | b);
+    }
+    
 }
